13e673e368a8edd7fe4782d62febae608042e32b
MercyRaz <123648128+MercyRaz@users.noreply.github.com>
2023-11-09 17:45:49 +0100

    initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { Paper, TextField } from "@material-ui/core";
+
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: "",
+    }
+
+    handleChange = (event) => this.setState({ searchTerm: event.target.value });
+
+    handleSubmit = (event) => {
+        const { searchTerm } = this.state;
+        const { onFormSubmit } = this.props;
+
+        onFormSubmit(searchTerm);
+
+        event.preventDefault();
+        
+    }
+
+    render() {
+        return(
+            <Paper elevetion={6} style={{ padding: "25px" }}>
+             <form onSubmit={this.handleSubmit}>
+                <TextField fullWidth label="search..." onChange={this.handleChange}/>
+             </form>
+
+            </Paper>
+        )
+    }
+}
+export default SearchBar;
